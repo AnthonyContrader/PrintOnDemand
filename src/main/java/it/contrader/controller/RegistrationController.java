61a1e3c63aeaf47262b74e387b08e8731c2a0af0
Francesco <francyssx@live.it>
2020-08-27 12:25:26 +0200

    Aggiornata view utente per Client e Item

---
+++ src/main/java/it/contrader/controller/RegistrationController.java
@@ -0,0 +1,40 @@
+package it.contrader.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import it.contrader.dto.UserDTO;
+import it.contrader.service.UserService;
+
+@Controller
+@RequestMapping("/register")
+public class RegistrationController {
+	
+	@Autowired
+	private UserService service;
+	
+		
+
+	@PostMapping("/insert")
+	public String insert( HttpServletRequest request, @RequestParam("username") String username,
+			@RequestParam("password") String password) {
+		UserDTO dto = new UserDTO();
+		dto.setUsername(username);
+		dto.setPassword(password);
+		dto.setUsertype("USER");
+		service.insert(dto);
+		setAll(request);
+		return "index";
+	}
+	
+	private void setAll(HttpServletRequest request) {
+		request.getSession().setAttribute("list", service.getAll());
+	}
+}
+
+
---
+++ src/main/java/it/contrader/dao/ClientRepository.java
@@ -0,0 +1,15 @@
+package it.contrader.dao;
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import it.contrader.model.Client;
+
+@Repository
+@Transactional
+public interface ClientRepository extends CrudRepository<Client, Long> {
+
+	Client findByIdclient(int idclient);
+}
