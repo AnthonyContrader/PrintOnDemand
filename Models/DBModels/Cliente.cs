6d18d0d06ece06ee3bf04e528cc80b3b1f8bd1af
riccardomarin99 <ric.marin@libero.it>
2020-09-18 17:21:05 +0200

    update

---
+++ Models/DBModels/Cliente.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace POD.Models.DBModels
+{
+    public partial class Cliente
+    {
+        public Cliente()
+        {
+            Articolo = new HashSet<Articolo>();
+        }
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public string Indirizzo { get; set; }
+        public int? Iduser { get; set; }
+
+        public virtual Utente IdNavigation { get; set; }
+        public virtual ICollection<Articolo> Articolo { get; set; }
+    }
+}
---
+++ Models/DBModels/Utente.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace POD.Models.DBModels
+{
+    public partial class Utente
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Usertype { get; set; }
+
+        public virtual Cliente Cliente { get; set; }
+    }
+}
